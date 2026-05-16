7abff6d2aee027baf31452368eede0deef7a3ab3
siddhant <siddhantj802@.com>
2026-05-16 09:38:42 +0530

    working with fetching data

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: false,
   images:{
-    domains: ["images.pexels.com"]
+    domains: ["images.pexels.com" , "dummyimage.com"]
   },
   reactCompiler: true,
+  cacheComponents: true,
 };
 
 export default nextConfig;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,28 +2,27 @@ import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+const getData = async ()=>{
+  const res = await fetch('https://jsonplaceholder.org/posts')
+  
+  if(!res.ok){
+    throw notFound()
   }
-
-  return res.json();
+  return res.json()
 }
-
 const Blog = async () => {
-  const data = await getData();
+  'use cache';
+  
+  const posts = await getData()
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {posts.map((item) => (
         <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
-              src={item.img}
+              src={item.image}
               alt=""
               width={400}
               height={250}
